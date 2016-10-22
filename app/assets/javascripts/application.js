fb936e637d6e7893b9cb803921d2872000cd631d
Alexander Gudulin <alexandr.gudulin@gmail.com>
2016-10-22 11:12:11 +0200

    fixup! hide alerts and make them absolute positioned

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,9 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require_tree .
+
+$(document).ready(function () {
+  setTimeout(function () {
+    $(".alert-success").alert('close')
+  }, 2500)
+})
