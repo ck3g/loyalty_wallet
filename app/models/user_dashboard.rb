f3ad88bda3b05a558d2de742f42075ac8bb45dd5
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-10-22 10:18:33 +0200

    Put real link inside QR code

--- app/models/user_dashboard.rb
+++ app/models/user_dashboard.rb
@@ -10,10 +10,17 @@ class UserDashboard
   end
 
   def qr_code
-    RQRCode::QRCode.new('Made in Wedding', size: 4, level: :h)
+    RQRCode::QRCode.new(qr_code_uri, size: 4, level: :h)
   end
 
   def view_name
     "user_dashboard"
+  end
+
+  private
+
+  def qr_code_uri
+    # TODO: get rid of hardcoded stuff
+    "http://loyaltywallet.2016.rubyrampage.com/users/#{user.id}/put-stamp"
   end
 end
