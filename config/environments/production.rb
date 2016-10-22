4ca4f699e7ead0a948edefc713dd0fbf7c70132e
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-10-22 23:06:52 +0200

    Revert "Try Mailgun instead of SendGrid"
    This reverts commit aa9d49cc576669f2171d04129f271daebd5b2e1b.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,14 +61,15 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: 'loyaltywallet.2016.rubyrampage.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'loyaltywallet.2016.rubyrampage.com' }
   config.action_mailer.smtp_settings = {
-    port: ENV['MAILGUN_SMTP_PORT'],
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: 'floating-caverns-63229.herokuapp.com',
-    authentication: :plain
+    address: 'smtp.sendgrid.net',
+    port: 587,
+    enable_starttls_auto: true,
+    authentication: :plain,
+    user_name: ENV['SENDGRID_USERNAME'],
+    password: ENV['SENDGRID_PASSWORD'],
+    domain: 'loyaltywallet.2016.rubyrampage.com'
   }
   config.action_mailer.delivery_method = :smtp
 
