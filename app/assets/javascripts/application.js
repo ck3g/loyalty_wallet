1b7d5cb6b90d47e8101eb89e16a232dcb3bdde40
Alexander Gudulin <alexandr.gudulin@gmail.com>
2016-10-22 16:57:10 +0200

    make header more cool, refactor js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,10 +16,30 @@
 //= require bootstrap
 //= require_tree .
 
-$(document).ready(function () {
-  $("img").lazyload()
+var NAVBAR_ACTIVE_CLASSNAME = 'navbar--active'
+var ALERTS_FADEOUT_DELAY_MS = 2500
 
-  setTimeout(function () {
-    $(".alert-success").alert('close')
-  }, 2500)
+$(document).ready(function () {
+  initLazyload()
+  initAlerts()
+
+  bindEvents()
 })
+
+function initAlerts () {
+  setTimeout(function () {
+    $('.alert-success').alert('close')
+  }, ALERTS_FADEOUT_DELAY_MS)
+}
+
+function initLazyload () {
+  $('img').lazyload()
+}
+
+function changeNavbarClassname () {
+  $('.navbar').toggleClass(NAVBAR_ACTIVE_CLASSNAME, $(window).scrollTop() > 200)
+}
+
+function bindEvents () {
+  $(window).on('scroll', changeNavbarClassname)
+}
