c69d7220c1ed1705f26b41c614b4a9cafa9e3b46
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2016-10-22 14:02:09 +0200

    Improve styles for navigation links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,11 @@
 module ApplicationHelper
+  def link_to_nav(title, path)
+    active_class = "active" if current_page? path
+    content_tag :li, class: active_class do
+      link_to title, path, class: "page-scroll"
+    end
+  end
+
   def current_stamps_count(total_count)
     "#{total_count % Stamp::PRIZE_STAMP_COUNT} / #{Stamp::PRIZE_STAMP_COUNT}"
   end
